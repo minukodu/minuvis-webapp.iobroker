0cfa9dfc0b835774bb09500c39311ab398b85830
Sepp Vallant <jo.va@gmx.at>
2026-08-15 13:57:37 +0200

    StatusBar color branding

--- android/app/src/main/java/com/minuvis/smarthome/MainActivity.java
+++ android/app/src/main/java/com/minuvis/smarthome/MainActivity.java
@@ -1,5 +1,39 @@
 package com.minuvis.smarthome;
 
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    // android:windowBackground (styles.xml, @color/navBarDark) faerbt im Edge-to-Edge-Modus
+    // die Flaeche HINTER Status- UND Navigation-Bar einheitlich ein einer Farbe.
+    // Fuer eine abweichende Status-Bar-Farbe wird hier ein eigener Scrim obendrauf gelegt,
+    // dessen Hoehe bei jeder Insets-Aenderung (Rotation etc.) an den echten Status-Bar-Inset angepasst wird.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View statusBarScrim = new View(this);
+        statusBarScrim.setBackgroundColor(ContextCompat.getColor(this, R.color.windowBackgroundTeal));
+        statusBarScrim.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, Gravity.TOP));
+
+        ViewGroup root = findViewById(android.R.id.content);
+        root.addView(statusBarScrim);
+
+        ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+            int topInset = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout()).top;
+            ViewGroup.LayoutParams lp = statusBarScrim.getLayoutParams();
+            lp.height = topInset;
+            statusBarScrim.setLayoutParams(lp);
+            statusBarScrim.bringToFront();
+            return insets;
+        });
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Capacitor } from '@capacitor/core';
+import { Capacitor, SystemBars, SystemBarsStyle, SystemBarType } from '@capacitor/core';
 import './index.css';
 import ConfigLoader from './components/ConfigLoader';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -15,5 +15,9 @@ root.render(
 // Service Worker nur im Browser (PWA), nicht in der nativen Capacitor-App
 if (!Capacitor.isNativePlatform()) {
   serviceWorkerRegistration.register();
+} else {
+  // Status-Bar-Hintergrund ist immer Teal (siehe MainActivity-Scrim) -> Icons immer hell.
+  // Navigation-Bar bleibt System-Standard (folgt Geraete-Dark/Light-Mode automatisch).
+  SystemBars.setStyle({ style: SystemBarsStyle.Dark, bar: SystemBarType.StatusBar });
 }
 
